319c238df873e7eb98cd9825ef551bf8e97dfcf1
Geoff Bucks <geoff@navigatingcancer.com>
2015-02-19 09:09:08 -0800

    Rewrite engine aware recognize path method to handle deep mounts
    For example, mounting care management at /clinics/:clinic_id/care wasn't
    working with the way this was previously written

--- lib/roles_on_routes/engine_aware_route_set.rb
+++ lib/roles_on_routes/engine_aware_route_set.rb
@@ -2,25 +2,29 @@ module RolesOnRoutes
   class EngineAwareRouteSet
 
     def self.for(app)
-      engines = app.railties.engines
-      new(app.routes, engines) 
+      new(app.routes)
     end
 
-    def initialize(main_routeset, engines=[])
+    def initialize(main_routeset)
       @main_routeset = main_routeset
-      @engines = engines
     end
 
     def recognize_path(path, environment={})
-      @engines.each do |engine|
-        # Find "mount" for the engine via named_routes on main_routeset
-        mount_route = @main_routeset.named_routes.routes.values.find { |r| r.app.to_s == engine.class.to_s }
-        next unless mount_route
+      route_match = @main_routeset.router.send(:find_routes, {
+        'PATH_INFO' => path,
+        'REQUEST_METHOD' => environment[:method]
+      }).first
 
-        engine_path = path.gsub(%r(^#{mount_route.path.spec.to_s}), '')
-        begin
-          return engine.routes.recognize_path(engine_path, environment)
-        rescue ActionController::RoutingError
+      if route_match
+        journey_route = route_match.last
+        engine = journey_route.app
+
+        if engine.respond_to?(:routes)
+          begin
+            engine_path = path.gsub(journey_route.path.to_regexp, '')
+            return engine.routes.recognize_path(engine_path, environment)
+          rescue ActionController::RoutingError
+          end
         end
       end
 
