2409b258b04866fc67dc148feec5477e1c26c3b6
pfenig <steven.fenigstein@gmail.com>
2015-11-16 14:47:35 -0800

    remove roles from routing scan so it doesnt mess with tests

--- lib/action_dispatch/routing/mapper_override.rb
+++ lib/action_dispatch/routing/mapper_override.rb
@@ -11,11 +11,22 @@ module ActionDispatch
             cleared_action_roles = @scope[:options].delete(:action_roles)
             yield if block_given?
             @scope[:options][:action_roles] = cleared_action_roles if cleared_action_roles.present?
+            self
           end
         end
       end
-
       include ClearsActionRolesFromChildScope
+
+      class Mapping
+        
+        def constraints_with_remove_roles(*args)
+          constraints = constraints_without_remove_roles(*args)
+          @conditions[:required_defaults].delete(:action_roles)
+          @conditions[:required_defaults].delete(:roles)
+          constraints
+        end
+        alias_method_chain :constraints, :remove_roles
+      end
     end
   end
 end
